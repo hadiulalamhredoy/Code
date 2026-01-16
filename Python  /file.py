4abd0e21d1c60b430db8315fa826f8785f8c5361
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2026-01-16 12:39:17 +0600

    Create file.py

---
+++ Python  /file.py
@@ -0,0 +1,9 @@
+def greet(): 
+    msg = " hello from inside the python"
+    print(msg)              
+greet() 
+
+def add(a, b):      
+    return a + b            
+result = add(5, 3)
+print("The sum is:", result)            
